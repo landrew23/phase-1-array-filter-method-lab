def186fb7358f15127f11cd2f50ac8278210475c
William Killett <williamkillettiii@yahoo.com>
2022-10-02 00:00:26 -0700

    commit tree 0e5683d8e69594c8d22fce8cb8a665cf1c75f129

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(driver, name){
+return driver.filter(word => word.toLowerCase() === name.toLowerCase())
+}
+function fuzzyMatch(driver, name){
+return driver.filter((word) => word.startsWith(name))
+}
+
+function matchName(driver, names){
+ return   driver.filter(driver => driver.name === names )
+}
